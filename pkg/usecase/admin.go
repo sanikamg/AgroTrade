ecc1c21c8b32c37d7d5791b4bf7129097da98c22
Sanika M G <sanikamg9@gmail.com>
2023-05-07 15:54:45 +0530

    twilio git ignore completed

---
+++ pkg/usecase/admin.go
@@ -0,0 +1,11 @@
+package usecase
+
+import interfaces "golang_project_ecommerce/pkg/repository/interface"
+
+type adminUsecase struct {
+	adminRepo interfaces.AdminRepository
+}
+
+func NewadminUsecase() {
+
+}
