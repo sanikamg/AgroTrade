57ca773cdc9b0d037b7ddbf9367b94fa170890e7
Sanika Mg <sanikamg9@gmail.com>
2023-08-08 10:03:25 +0530

    swagger

--- pkg/api/handler/user.go
+++ pkg/api/handler/user.go
@@ -127,7 +127,7 @@ func (cr *UserHandler) VerifyOTP(c *gin.Context) {
 
 // Registration godoc
 //
-//	@summary		api for complete registration hii
+//	@summary		api for complete registration hii how are you
 //	@description	Enter user details
 //	@tags			SignUp For User
 //	@Param			inputs	body	domain.Users{}	true	"Input Field"
